8b91d52225e14517221d85ef9fc9e42427f148ce
Mohamed Nheri <nherimohamed@gmail.com>
2024-02-24 15:11:16 -0500

    Soft assert challenege

--- src/test/java/SoftAssertHardAssert/ChallenegSoftAssert.java
+++ src/test/java/SoftAssertHardAssert/ChallenegSoftAssert.java
@@ -1,5 +1,14 @@
 package SoftAssertHardAssert;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class ChallenegSoftAssert {
 
     /**
@@ -10,6 +19,34 @@ public class ChallenegSoftAssert {
      *
      * -- Use Soft Assert
      */
+
+
+    @Test
+    void verifyLabelsInMainPage() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+        Thread.sleep(5000);
+        SoftAssert softAssert = new SoftAssert();
+
+        String expectedBackpackLabel = "Sauce Labs";
+        String actualBackpackLabel = driver.findElement(By.xpath("//div[.='Sauce Labs Backpack']")).getText();
+        softAssert.assertEquals(actualBackpackLabel , expectedBackpackLabel);
+
+
+        //===========================
+        System.out.println("This will not print if its hard assert");
+        String expectedBikeLightLabel = "Sauce Labs Bike Light";
+        String actualBikeLightLabel = driver.findElement(By.xpath("//div[.='Sauce Labs Bike Light']")).getText();
+        softAssert.assertEquals(actualBikeLightLabel , expectedBikeLightLabel);
+
+        softAssert.assertAll();
+    }
+
 
 
 
